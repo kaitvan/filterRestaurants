67c5e63010b555dd3958538bfedbb7dfe9a517c9
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2020-06-25 10:54:07 -0500

    Initial Commit

---
+++ filterRestaurants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  filterRestaurants
+//
+//  Created by Kaitlyn VanHook on 6/25/20.
+//  Copyright © 2020 Kaitlyn VanHook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
